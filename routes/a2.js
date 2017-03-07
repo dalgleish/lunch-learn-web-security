647e2c1593d4c516296bed0c9ba7f1740426e670
Sara Gudeman <sgudeman982@gmail.com>
2017-03-06 20:40:58 -0800

    adds search box for reflected xss

--- routes/a2.js
+++ routes/a2.js
@@ -2,17 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('xxs', { title: 'XXS Attack' });
+  res.render('xxs', { title: 'XSS Attack' });
 });
 
 router.get('/reflected', function(req, res, next) {
 	var searchQuery = req.query.search;
-  res.send(searchQuery);
+	if (searchQuery) {
+		res.send(searchQuery);
+	} else {
+		res.render('xxs-reflected', { title: 'Reflected XSS Attack'})
+	}
 });
 
 router.get('/stored', function(req, res, next) {
-    store.comment += 'Message board comment<script>alert(\'hi\')<\/script>';
-    res.send(JSON.stringify(store));	
+  store.comment += 'Message board comment<script>alert(\'hi\')<\/script>';
+  res.send(JSON.stringify(store));
 });
 
 var store = {
